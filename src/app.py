f41341cb36bc8442852869c168f4452b859a5493
Léo Dutertre <ldutertreladuree@gmail.com>
2024-09-12 18:01:55 +0200

    change pwd

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
-from dash import html, Dash, page_registry, page_container, callback, Input, Output, callback_context
+from dash import html, Dash, page_registry, page_container, callback, Input, Output, callback_context, dcc
 import dash_bootstrap_components as dbc
+from datetime import datetime
 
 app = Dash(__name__,
     use_pages=True,
@@ -11,9 +12,36 @@ n_clicks_closing_sidebar = []
 server = app.server
 
 
+link_footer = {
+    'color':'white',
+    'fontWeight': 'normal'
+}
+
+
+
 footer = html.Div([
-    html.Div(html.Img(src='assets/img/mailto.png')),
-    html.Div('lala')
+    
+    html.Div([
+
+        html.A([
+            html.Img(src='assets/img/in.png', style={'width':'36px'}),
+            'Léo Dutertre-Ladurée',            
+        ], href='https://www.linkedin.com/in/leo-dutertre-laduree-502a5689/', target='_blank', className='l', style = link_footer)
+    ], style={'display':'flex', 'alignItems':'center'}),
+    
+    html.Div([
+        html.A([
+            html.Img(src='assets/img/mailto.png', style={'width':'36px'}),
+            'ldutertreladuree@gmail.com'
+        ], href='mailto:ldutertreladuree@gmail.com', target='_blank', className='l', style = link_footer)
+    ], style={'display':'flex', 'alignItems':'center'}),
+    
+    html.Div([
+        html.Img(src='assets/img/calendar.png', style={'width':'34px'}),
+        html.Div(datetime.today().strftime('%d %B %Y'))        
+    ], style={'display':'flex', 'alignItems':'center'}),
+    
+
     
     ], className='footer')
 
@@ -37,14 +65,13 @@ content = html.Div(page_container, id='body', className='body')
 navbar = html.Div([
     html.Div(html.Img(src='assets/img/logo.png', className="logo"), style={'text-align':"center"}),
     dbc.Button("llll", color = "primary", id = "closing-sidebar", className="btn-close-sidebar"),
-    html.A(html.Img(src='assets/img/cezane.jpg', className="cezane"), className="circle", href="https://www.linkedin.com/in/leo-dutertre-laduree-502a5689/", target="_blank")
+    html.A(html.Img(src='assets/img/cezane.jpg', className="cezane"), className="circle", href="https://dash.plotly.com/", target="_blank")
 ], className='navbar')
 
 app.layout = html.Div([navbar, sidebar, content], id="app", className="all")
 
 sd_open = [[navbar, sidebar, content],"sidebar","body"]
 sd_close = [[navbar, sidebar_closed, content],"sidebarClose","bodyFull"]
-
 
 
 @callback(
@@ -62,9 +89,7 @@ def closing_sidebar(n_clicks):
             active = sd_open 
         else:
             active = sd_close
-    return active            
-
-
+    return active
 
 if __name__ == "__main__":
     app.run(debug=True)
